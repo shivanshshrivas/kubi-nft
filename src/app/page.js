6f8963922d0428a52a3854788f350544222a51a4
Shivansh Shrivas <shrivasshivansh@gmail.com>
2024-04-09 17:37:58 -0500

    fixed error

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
       <AmoyProvider>
         <Box textAlign="center" marginTop="4">
           <Heading as="h1" size="2xl" marginBottom="8">
-            Shivansh's NFT Gallery
+            Shivansh NFT Gallery
           </Heading>
           <Box my={4}>
             {" "}
@@ -71,7 +71,7 @@ export default function Home() {
           padding="8"
         >
           <Text fontSize="md" marginBottom="2">
-            Made with 🔥 by the University of Kansas Blockchain Institute
+            Made with &nbsp;🔥&nbsp; by the University of Kansas Blockchain Institute
           </Text>
           <Link href={tutorialUrl} isExternal>
             <Button colorScheme="blue" variant="outline" size="sm">
